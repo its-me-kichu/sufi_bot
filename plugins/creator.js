03fe85a3d6d4ac66c059721c3cebb290b335b8e2
Prince Rudh <91786052+Princerudh@users.noreply.github.com>
2021-10-19 19:47:35 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '919995390942', 'Nurutomo', m)
-  this.sendContact(m.chat, '919995390942', 'Nurutomo', m)
+  // F this.sendContact(m.chat, '919645139960', 'Nurutomo', m)
+  this.sendContact(m.chat, '919645139960', 'Nurutomo', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
