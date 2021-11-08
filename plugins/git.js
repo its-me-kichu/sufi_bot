0786da5c282f6c6ea66087d3171e69dc0b4b1b7f
PrinceRudh <93796776+PrinceRudh@users.noreply.github.com>
2021-11-09 01:48:34 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -5,51 +5,15 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var r_text = new Array ();
-    
-    
-   
-  r_text[0] = "https://i.postimg.cc/L4Ctzhvh/Prince-Rudh.jpg";
-    
-    
-    var i = Math.floor(1*Math.random())
+    var skl = await axios.get("https://i.postimg.cc/L4Ctzhvh/Prince-Rudh.jpg", { responseType: 'arraybuffer' })
 
-    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Github* *(setup)* : *https://github.com/PrinceRudh/PrinceRudh*
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `╭──────────────────╮
-   Prince Rudh
-╭──────────────────╯
-│
-│
-│
-│
-│ ▢ *ᴏᴡɴᴇʀ* : Prince Rudh
-│
-│ ▢ *ɴᴜᴍʙᴇʀ* : wa.me/919895339960
-│
-│ ▢ *ɪɴsᴛᴀɢʀᴀᴍ* :https://instagram.com/princerudh
-│
-│ ▢ *ʏᴛ* :https://youtube.com/c/PrinceRudh
-│
-│ ▢ *ɢɪᴛʜᴜʙ* :https://github.com/Princerudh/PrinceRudh
-│
-│ ▢ *ᴀᴜᴅɪᴏ ᴄᴏᴍɴᴅs* :https://github.com/Princerudh/PrinceRudh/tree/master/uploads
-│
-│ ▢ *sᴛɪᴄᴋᴇʀ ᴄᴏᴍɴᴅs* :https://github.com/Princerudh/PrinceRudh/tree/master/uploads
-│
-│
-│
-│      ▎▍▌▌▉▏▎▌▉▐▏▌▎
-│      ▎▍▌▌▉▏▎▌▉▐▏▌▎
-│       ©919895339960
-│
-╰──────────────────╮
-│ 
-│
-│   ❏ 𝘤𝘰𝘥𝘦𝘥 𝘣𝘺 *Prince Rudh* ❏
-╰──────────────────╯
+*Audio comnds* : *https://github.com/PrinceRudh/PrinceRudh/tree/master/uploads*
+
+*Sticker cmnds* : *https://github.com/PrinceRudh/PrinceRudh/tree/master/uploads*
 `}) 
 
 }));
